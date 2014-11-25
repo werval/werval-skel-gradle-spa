72ff2fbe491da61619918829d607a73e9f504ea7
Paul Merlin <paul@nosphere.org>
2014-11-25 19:07:55 +0100

    Javascript sample refinement

--- src/main/web/scripts/main.js
+++ src/main/web/scripts/main.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // Using jQuery here for the example's sake, feel free to remove it
-window.$ = window.jQuery = require("jquery");
-window.$.getJSON("/greeting/QiWeb Single Page Application", function (data) {
-    window.$("#spa").append("<h1>" + data.message + "</h1>");
+var $ = require("jquery");
+
+$.getJSON("/greeting/QiWeb Single Page Application", function (data) {
+    $("#spa").append("<h1>" + data.message + "</h1>");
 });
